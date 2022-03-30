d86a7898043d7992100f3f17523363890b0b419a
LiuFu <983188204@qq.com>
2022-03-30 23:22:02 +0800

    2

---
+++ src/main/java/com/liufu/store/interceptor/LoginInterceptor.java
@@ -0,0 +1,38 @@
+package com.liufu.store.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    /**
+     * 检测全局session中是否有uid数据，如果有则放行，如果没有，重定向到登录页面
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object uid = request.getSession().getAttribute("uid");
+        if(uid == null){// 说明用户没有登录过系统，则重定向到login.html页面
+            response.sendRedirect("/web/login.html");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
